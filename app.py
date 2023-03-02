e6d2c0103f6e5746a699e0a5c126664485e4a644
Mohit Sharma <bmohits0203@gmail.com>
2023-03-02 12:33:31 +0530

    frontend Added

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template, request
 from database.database import getOrders
+from wtforms import Form, TextAreaField, validators, SelectField
+import pickle
 app = Flask(__name__)
- 
+
+
+class LoginForm(Form):
+    phone = TextAreaField('',[validators.DataRequired(),validators.length(10)])
+    password = TextAreaField('',[validators.DataRequired(),validators.length(min=8)])
+    role = SelectField('',[validators.DataRequired()])
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        return render_template('./admin/index.html')    
  
 @app.route('/')
-def index():
-    orders=str(getOrders())
-    return orders
+def login():
+    form = LoginForm(request.form)
+    return render_template('login.html')
  
  
+@app.route('/signup')
+def signup():
+  return render_template('signup.html') 
+   
 if __name__ == '__main__':
     app.run()
