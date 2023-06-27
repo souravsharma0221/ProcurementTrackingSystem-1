da148e98819d2f5289bf618ae7cd2981ae13994b
sourabh <sourabh727272@gmail.com>
2023-06-27 17:19:32 +0530

    FirstCommit

--- database/database.py
+++ database/database.py
@@ -2,6 +2,8 @@ import os
 from sqlalchemy import create_engine
 
 db_connection_string=os.environ['DB_CONN_STR']
+db_connection_string = 'your_connection_string_here'
+
 
 engine = create_engine(db_connection_string,connect_args={
     "ssl":{
